f5d466842984a4481a2c8a8776eb3a4bc14867ba
Matt Williamson <mwilliamson@vmox.com>
2022-02-25 05:50:43 -0600

    master branch

--- mwilliamson_portfolio_python/settings.py
+++ mwilliamson_portfolio_python/settings.py
@@ -78,6 +78,7 @@ WSGI_APPLICATION = 'mwilliamson_portfolio_python.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
