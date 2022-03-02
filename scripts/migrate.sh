23edaa5291a113b496e71a7e8dd10af1b109a587
Matt Williamson <mwilliamson@vmox.com>
2022-03-01 21:46:03 -0600

    Fixing script executables

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 cd /var/www/personal_portfolio_python
 source /var/www/personal_portfolio_python/mwilliamson_portfolio_python/env/bin/activate
 python3 manage.py makemigrations
--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 ps auxw | grep runserver | awk '{print $2}' | xargs kill
