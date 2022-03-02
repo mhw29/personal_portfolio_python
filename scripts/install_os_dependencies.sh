885a3dde4c25e34b72b9d9b23702584e0a3fdfa4
Matt Williamson <mwilliamson@vmox.com>
2022-03-01 20:45:45 -0600

    Fix os depend scripts

--- scripts/install_os_dependencies.sh
+++ scripts/install_os_dependencies.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
-apt-get install -y python-psycopg2 postgresql libncurses5-dev libffi libffi-devel libxml2-devel libxslt-devel libxslt1-dev
-apt-get install -y postgresql-libs postgresql-devel python-lxml python-devel gcc patch python-setuptools
-apt-get install -y gcc-c++ flex epel-release nginx supervisor
+apt install python3-pip
+apt-get install postgresql postgresql-contrib
+python3 -m venv env
+apt install python3.8-venv
+pip install psycopg2-binary
+apt install libpq-dev
+apt-get install libffi-dev
+apt-get install python3-dev
+apt-get install build-essential autoconf libtool pkg-config python-opengl python-pil python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libssl-dev
+apt-get install libpq-dev python-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libffi-dev
+apt-get install python3.8-dev
+apt-get install gcc
+apt-get install libxml2-dev libxslt1-dev
+pip install wheel
+apt-get install -y libssl-dev
+apt-get install libmysqlclient-dev
+apt-get install unixodbc unixodbc-dev
+apt-get install python3-tk
 #! /etc/init.d/nginx stop
