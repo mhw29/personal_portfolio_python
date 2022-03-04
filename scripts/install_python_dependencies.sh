5f0204338396f9747d698427cbceeb29beec37ce
Matt Williamson <mwilliamson@vmox.com>
2022-03-03 20:26:33 -0600

    .

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 chown ubuntu:ubuntu /var/www
-virtualenv /var/www/personal_portfolio_python/mwilliamson_portfolio_python/env
-chown ubuntu:ubuntu /var/www/personal_portfolio_python/mwilliamson_portfolio_python/env
-chown ubuntu:ubuntu /var/www/personal_portfolio_python/mwilliamson_portfolio_python/env/*
-source /var/www/personal_portfolio_python/mwilliamson_portfolio_python/env/bin/activate
+virtualenv /var/www/personal_portfolio_python/env
+chown ubuntu:ubuntu /var/www/personal_portfolio_python/env
+chown ubuntu:ubuntu /var/www/personal_portfolio_python/env/*
+source /var/www/personal_portfolio_python/env/bin/activate
 pip install -r /var/www/personal_portfolio_python/requirements.txt
 chmod +x /var/www/personal_portfolio_python/scripts/*.sh
 pip install gunicorn
--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /var/www/personal_portfolio_python
-source /var/www/personal_portfolio_python/mwilliamson_portfolio_python/env/bin/activate
+source /var/www/personal_portfolio_python/env/bin/activate
 python3 manage.py makemigrations
 python3 manage.py migrate
