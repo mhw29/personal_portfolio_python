813decf968c06efda31f288747c4671b917c1551
Matt Williamson <mwilliamson@vmox.com>
2022-03-03 20:41:50 -0600

    .

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,4 +2,4 @@
 cd /var/www/personal_portfolio_python
 source /www/personal_portfolio_python/env/bin/activate
 #python3 manage.py runserver 0.0.0.0:8000 &
-gunicorn --bind 0.0.0.0:8000 mwilliamson_portfolio_python.wsgi
+#gunicorn --bind 0.0.0.0:8000 mwilliamson_portfolio_python.wsgi
